ad890cd9a79b24fc293fb067e50b445928739461
Eric Oliveira <oiericoi@hotmail.com>
2023-03-03 16:16:49 -0300

    refactor: add validators in routes

--- src/routers/building.routes.ts
+++ src/routers/building.routes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
 import { buildingController } from '../modules/building/controller/buildingController';
+import { buildingValidator } from '../validators/building';
+import { handleValidator } from '../validators/handleValidator';
 
 export const buildingRouter = Router();
 
-buildingRouter.post('/store', buildingController.store);
-buildingRouter.get('/', buildingController.readAll);
-buildingRouter.get('/:id', buildingController.read);
-buildingRouter.put('/edit', buildingController.edit);
+buildingRouter.post('/store',
+  buildingValidator.create,
+  handleValidator,
+  buildingController.store
+);
+
+buildingRouter.get('/',
+  buildingController.readAll
+);
+
+buildingRouter.get('/:id',
+  buildingController.read
+);
+
+buildingRouter.put('/edit',
+  buildingValidator.id,
+  buildingValidator.create,
+  handleValidator,
+  buildingController.edit
+);
