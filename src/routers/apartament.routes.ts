62492f01c6bd05ee0c0b2b3ff79b2487d523b4a8
Eric Oliveira <oiericoi@hotmail.com>
2023-03-03 14:40:41 -0300

    refactor: modify apartment router

--- src/routers/apartament.routes.ts
+++ src/routers/apartament.routes.ts
@@ -4,6 +4,8 @@ import { apartmentController } from '../modules/apartment/controller/apartmentCo
 export const apartmentRouter = Router();
 
 apartmentRouter.post('/store', apartmentController.store);
+apartmentRouter.post('/rentapartment/:id', apartmentController.rentApartment);
+apartmentRouter.post('/removerenter/:id', apartmentController.removeRenter);
 apartmentRouter.get('/', apartmentController.readAll);
 apartmentRouter.get('/availables', apartmentController.readAvailables);
 apartmentRouter.get('/:id', apartmentController.read);
