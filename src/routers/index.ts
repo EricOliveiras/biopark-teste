c5c9420595a169746451cd62f4c4a731056c4b92
Eric Oliveira <oiericoi@hotmail.com>
2023-03-03 14:41:04 -0300

    feat: add renter router in index routes

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { rootRouter } from './root.routes';
 import { apartmentRouter } from './apartament.routes';
 import { buildingRouter } from './building.routes';
+import { renterRouter } from './renter.routes';
 
 export const routes = Router();
 
 routes.use('/building', buildingRouter);
 routes.use('/apartment', apartmentRouter);
+routes.use('/renter', renterRouter);
 routes.use('/', rootRouter);
