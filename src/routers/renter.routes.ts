ff9bbb9d40570db8f2793e2c435d3762e3a17777
Eric Oliveira <oiericoi@hotmail.com>
2023-03-03 22:04:43 -0300

    refactor: add validators in routes

--- src/routers/renter.routes.ts
+++ src/routers/renter.routes.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express';
 import { renterController } from '../modules/renter/controller/renterController';
+import { renterValidator } from '../validators/renter';
+import { handleValidator } from '../validators/handleValidator';
 
 export const renterRouter = Router();
 
-renterRouter.post('/store', renterController.strore);
-renterRouter.get('/', renterController.readAll);
-renterRouter.get('/:id', renterController.read);
-renterRouter.put('/edit', renterController.edit);
+renterRouter.post('/store',
+  renterValidator.create,
+  handleValidator,
+  renterController.strore
+);
+
+renterRouter.get('/',
+  renterController.readAll
+);
+
+renterRouter.get('/:id',
+  renterValidator.id,
+  handleValidator,
+  renterController.read
+);
+renterRouter.put('/edit',
+  renterValidator.idBody,
+  renterValidator.create,
+  handleValidator,
+  renterController.edit
+);
