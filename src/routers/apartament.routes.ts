192cf76ebd6b9f7075a4ad6d8ec9508b40b3dc2a
Eric Oliveira <oiericoi@hotmail.com>
2023-03-03 22:04:53 -0300

    refactor: add validators in routes

--- src/routers/apartament.routes.ts
+++ src/routers/apartament.routes.ts
@@ -1,12 +1,47 @@
 import { Router } from 'express';
 import { apartmentController } from '../modules/apartment/controller/apartmentController';
+import { apartmentValidator } from '../validators/apartment';
+import { handleValidator } from '../validators/handleValidator';
+import { renterValidator } from '../validators/renter';
 
 export const apartmentRouter = Router();
 
-apartmentRouter.post('/store', apartmentController.store);
-apartmentRouter.post('/rentapartment/:id', apartmentController.rentApartment);
-apartmentRouter.post('/removerenter/:id', apartmentController.removeRenter);
-apartmentRouter.get('/', apartmentController.readAll);
-apartmentRouter.get('/availables', apartmentController.readAvailables);
-apartmentRouter.get('/:id', apartmentController.read);
-apartmentRouter.put('/edit', apartmentController.edit);
+apartmentRouter.post('/store',
+  apartmentValidator.create,
+  handleValidator,
+  apartmentController.store
+);
+
+apartmentRouter.post('/rentapartment/:id',
+  apartmentValidator.id,
+  renterValidator.create,
+  handleValidator,
+  apartmentController.rentApartment
+);
+
+apartmentRouter.post('/removerenter/:id',
+  apartmentValidator.id,
+  handleValidator,
+  apartmentController.removeRenter
+);
+
+apartmentRouter.get('/',
+  apartmentController.readAll
+);
+
+apartmentRouter.get('/availables',
+  apartmentController.readAvailables
+);
+
+apartmentRouter.get('/:id',
+  apartmentValidator.id,
+  handleValidator,
+  apartmentController.read
+);
+
+apartmentRouter.put('/edit',
+  apartmentValidator.idBody,
+  apartmentValidator.update,
+  handleValidator,
+  apartmentController.edit
+);
