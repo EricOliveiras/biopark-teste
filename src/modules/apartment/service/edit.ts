5b9b3f65fb0010b1772601d73b4894c7ea603691
Eric Oliveira <oiericoi@hotmail.com>
2023-03-06 10:04:58 -0300

    refactor: modify edit apartment

--- src/modules/apartment/service/edit.ts
+++ src/modules/apartment/service/edit.ts
@@ -11,14 +11,5 @@ export const edit = async (apartament: Partial<Apartment>): Promise<Apartment> =
     throw new HttpException(404, 'Apartment not registered or does not exist');
   }
 
-  const readApartmentNumber = await apartamentRepository.readByNumber(
-    <string>apartament.buildingId,
-    <string>apartament.number
-  );
-
-  if (readExistingApartment.number !== <string>apartament.number && readApartmentNumber) {
-    throw new HttpException(404, 'There is already an apartment in this building registered with this number');
-  }
-
   return await apartamentRepository.edit(<Apartment>apartament);
 };
