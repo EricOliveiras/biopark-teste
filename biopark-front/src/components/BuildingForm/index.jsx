41934c62ed0732c6eab0df05c35c0559188452ab
Eric Oliveira <oiericoi@hotmail.com>
2023-03-06 10:25:42 -0300

    feat: add building form component

---
+++ biopark-front/src/components/BuildingForm/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { buildingAPI } from '../../services/building';
+
+import './style.css';
+
+function BuildingForm() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async ({ name, address, numberOfApartments }) => {
+    const numApt = parseInt(numberOfApartments);
+    const response = await buildingAPI.create({
+      name: name,
+      address: address,
+      numberOfApartments: numApt,
+    });
+
+    if (response.status === 201) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Adicionar edifício</h2>
+        <div className="form-input">
+          <label htmlFor="name">Nome do edifício</label>
+          <input id="name" {...register('name', { required: true })} />
+          {errors.name && <span>Este campo é obrigatório</span>}
+        </div>
+        <div className="form-input">
+          <label htmlFor="address">Endereço</label>
+          <input id="address" {...register('address', { required: true })} />
+          {errors.address && <span>Este campo é obrigatório</span>}
+        </div>
+        <div className="form-input">
+          <label htmlFor="numberOfApartments">Número de apartamentos</label>
+          <input
+            id="numberOfApartments"
+            type="number"
+            {...register('numberOfApartments', { required: true, min: 1 })}
+          />
+          {errors.numberOfApartments &&
+            errors.numberOfApartments.type === 'required' && (
+            <span>Este campo é obrigatório</span>
+          )}
+          {errors.numberOfApartments &&
+            errors.numberOfApartments.type === 'min' && (
+            <span>O número mínimo de apartamentos é 1</span>
+          )}
+        </div>
+
+        <button type="submit">Registrar edifício</button>
+      </form>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default BuildingForm;
